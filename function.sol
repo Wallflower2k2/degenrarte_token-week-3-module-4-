f02bd2d2a033e3c32604abfe6db31a93a8383998
Wallflowerk2k2 <gaurikaushal18@gmail.com>
2023-07-22 15:58:26 +0530

    upload_code

---
+++ function.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HardHat {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Burn(address indexed account, uint256 amount);
+    event Mint(address indexed account, uint256 amount);
+
+    constructor(string memory tokenName, string memory tokenSymbol, uint8 tokenDecimals, uint256 initialSupply) {
+        name = tokenName;
+        symbol = tokenSymbol;
+        decimals = tokenDecimals;
+        _totalSupply = initialSupply * (10 ** uint256(tokenDecimals));
+        _balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) external returns (bool) {
+        require(amount <= _balances[msg.sender], "Insufficient balance");
+        _balances[msg.sender] -= amount;
+        _totalSupply -= amount;
+        emit Burn(msg.sender, amount);
+        emit Transfer(msg.sender, address(0), amount);
+        return true;
+    }
+
+    function mint(address account, uint256 amount) external returns (bool) {
+        require(account != address(0), "Invalid account address");
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Mint(account, amount);
+        emit Transfer(address(0), account, amount);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "Invalid sender address");
+        require(recipient != address(0), "Invalid recipient address");
+        require(amount <= _balances[sender], "Insufficient balance");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+}
